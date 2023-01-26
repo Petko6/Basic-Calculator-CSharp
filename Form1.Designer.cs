560b99c267ed169cadf020e5a619550edd7b2252
Petr Procházka <petrprochazka06@email.cz>
2023-01-26 16:40:39 +0100

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Kalkulacka2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonRovnaSe = new System.Windows.Forms.Button();
+            this.TextovéPole = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonFloat = new System.Windows.Forms.Button();
+            this.buttonDeleni = new System.Windows.Forms.Button();
+            this.buttonNasobeni = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 119);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(148, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 119);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(293, 253);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 119);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(3, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 119);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(148, 378);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 119);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(293, 378);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 119);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Location = new System.Drawing.Point(3, 503);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 119);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Location = new System.Drawing.Point(148, 503);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 119);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Location = new System.Drawing.Point(293, 503);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(139, 119);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.numberButton);
+            // 
+            // button0
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.button0, 2);
+            this.button0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button0.Location = new System.Drawing.Point(3, 628);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(284, 122);
+            this.button0.TabIndex = 18;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.numberButton);
+            // 
+            // buttonRovnaSe
+            // 
+            this.buttonRovnaSe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRovnaSe.Location = new System.Drawing.Point(438, 503);
+            this.buttonRovnaSe.Name = "buttonRovnaSe";
+            this.tableLayoutPanel1.SetRowSpan(this.buttonRovnaSe, 2);
+            this.buttonRovnaSe.Size = new System.Drawing.Size(141, 247);
+            this.buttonRovnaSe.TabIndex = 17;
+            this.buttonRovnaSe.Text = "=";
+            this.buttonRovnaSe.UseVisualStyleBackColor = true;
+            this.buttonRovnaSe.Click += new System.EventHandler(this.buttonEquals);
+            // 
+            // TextovéPole
+            // 
+            this.TextovéPole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.TextovéPole, 3);
+            this.TextovéPole.Font = new System.Drawing.Font("Times New Roman", 58F);
+            this.TextovéPole.Location = new System.Drawing.Point(3, 3);
+            this.TextovéPole.Name = "TextovéPole";
+            this.TextovéPole.ReadOnly = true;
+            this.TextovéPole.Size = new System.Drawing.Size(429, 119);
+            this.TextovéPole.TabIndex = 1;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonPlus.Location = new System.Drawing.Point(438, 253);
+            this.buttonPlus.Name = "buttonPlus";
+            this.tableLayoutPanel1.SetRowSpan(this.buttonPlus, 2);
+            this.buttonPlus.Size = new System.Drawing.Size(141, 244);
+            this.buttonPlus.TabIndex = 10;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.Operator);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonDelete.Location = new System.Drawing.Point(438, 3);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(141, 119);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "<";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.DeleteOne);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonClear.Location = new System.Drawing.Point(3, 128);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(139, 119);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "C";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.Clear);
+            // 
+            // buttonFloat
+            // 
+            this.buttonFloat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonFloat.Location = new System.Drawing.Point(293, 628);
+            this.buttonFloat.Name = "buttonFloat";
+            this.buttonFloat.Size = new System.Drawing.Size(139, 122);
+            this.buttonFloat.TabIndex = 19;
+            this.buttonFloat.Text = ",";
+            this.buttonFloat.UseVisualStyleBackColor = true;
+            this.buttonFloat.Click += new System.EventHandler(this.numberButton);
+            // 
+            // buttonDeleni
+            // 
+            this.buttonDeleni.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonDeleni.Location = new System.Drawing.Point(148, 128);
+            this.buttonDeleni.Name = "buttonDeleni";
+            this.buttonDeleni.Size = new System.Drawing.Size(139, 119);
+            this.buttonDeleni.TabIndex = 4;
+            this.buttonDeleni.Text = "/";
+            this.buttonDeleni.UseVisualStyleBackColor = true;
+            this.buttonDeleni.Click += new System.EventHandler(this.Operator);
+            // 
+            // buttonNasobeni
+            // 
+            this.buttonNasobeni.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonNasobeni.Location = new System.Drawing.Point(293, 128);
+            this.buttonNasobeni.Name = "buttonNasobeni";
+            this.buttonNasobeni.Size = new System.Drawing.Size(139, 119);
+            this.buttonNasobeni.TabIndex = 5;
+            this.buttonNasobeni.Text = "*";
+            this.buttonNasobeni.UseVisualStyleBackColor = true;
+            this.buttonNasobeni.Click += new System.EventHandler(this.Operator);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonMinus.Location = new System.Drawing.Point(438, 128);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(141, 119);
+            this.buttonMinus.TabIndex = 6;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.Operator);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.TextovéPole, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonRovnaSe, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.buttonPlus, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonFloat, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.buttonNasobeni, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonDelete, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonMinus, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonClear, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.buttonDeleni, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button0, 0, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(582, 753);
+            this.tableLayoutPanel1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(582, 753);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Kalkulačka";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonRovnaSe;
+        private System.Windows.Forms.TextBox TextovéPole;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonFloat;
+        private System.Windows.Forms.Button buttonDeleni;
+        private System.Windows.Forms.Button buttonNasobeni;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kalkulacka2._0
+{
+    public partial class Form1 : Form
+    {
+        string firstValue;
+        string secondValue;
+        string finalValue;
+        string calculationOperator;
+        bool firstValueCompleted = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numberButton(object sender, EventArgs e)
+        {
+            string buttonValue = (sender as Button).Text;
+            if (firstValueCompleted == false)
+            {
+                firstValue = firstValue + buttonValue;
+                TextovéPole.Text = firstValue;
+            }
+            else
+            {
+                if (calculationOperator == null)
+                {
+                    firstValueCompleted = false;
+                    firstValue = null;
+                    firstValue = firstValue + buttonValue;
+                    TextovéPole.Text = firstValue;
+                }
+                else
+                {
+                    secondValue = secondValue + buttonValue;
+                    TextovéPole.Text = secondValue;
+                }
+
+            }
+
+        }
+
+        private void Equals()
+        {
+            if (firstValue != null && secondValue != null)
+            {
+                finalValue = Calculation();
+                TextovéPole.Text = finalValue;
+                firstValue = finalValue;
+                secondValue = null;
+                finalValue = null;
+            }
+            else if (firstValue != null && secondValue == null)
+            {
+                firstValueCompleted = true;
+                TextovéPole.Text = null;
+            }
+        }
+
+        private string Calculation()
+        {
+            if (calculationOperator == "+")
+            {
+                return Convert.ToString(Double.Parse(firstValue) + Double.Parse(secondValue));
+            }
+            if (calculationOperator == "-")
+            {
+                return Convert.ToString(Double.Parse(firstValue) - Double.Parse(secondValue));
+            }
+
+            if (calculationOperator == "/")
+            {
+                return Convert.ToString(Double.Parse(firstValue) / Double.Parse(secondValue));
+            }
+            if (calculationOperator == "*")
+            {
+                return Convert.ToString(Double.Parse(firstValue) * Double.Parse(secondValue));
+            }
+            return null;
+        }
+
+        private void Operator(object sender, EventArgs e)
+        {
+
+            Equals();
+            calculationOperator = (sender as Button).Text;
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            firstValue = null;
+            secondValue = null;
+            finalValue = null;
+            calculationOperator = null;
+            firstValueCompleted = false;
+            TextovéPole.Text = null;
+        }
+
+        private void DeleteOne(object sender, EventArgs e)
+        {
+            if (firstValueCompleted == false)
+            {
+                if (firstValue.Length > 0)
+                {
+                    firstValue = firstValue.Remove(firstValue.Length - 1);
+                }
+                TextovéPole.Text = firstValue;
+            }
+            else
+            {
+                if (secondValue.Length > 0)
+                {
+                    secondValue = secondValue.Remove(secondValue.Length - 1);
+                }
+                TextovéPole.Text = secondValue;
+            }
+        }
+
+        private void buttonEquals(object sender, EventArgs e)
+        {
+            Equals();
+            calculationOperator = null;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
